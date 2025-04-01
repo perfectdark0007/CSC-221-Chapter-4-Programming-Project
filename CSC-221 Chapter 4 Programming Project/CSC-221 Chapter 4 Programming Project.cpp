d67d1dbde404f6be2fa91c9715224e02b5e78393
perfectdark0007 <woodardwjun@gmail.com>
2025-03-31 20:21:16 -0400

    Add project files.

---
+++ CSC-221 Chapter 4 Programming Project/CSC-221 Chapter 4 Programming Project.cpp
@@ -0,0 +1,10 @@
+// William Woodard's CSC-221 Chapter 4 Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello Professor Schaffner, hopefully this works!\n";
+}
+
